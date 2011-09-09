e9e9b0bda2819aad04ced894995f31e3579c8e8b
Michelangelo Altamore <michelangelo@altamore.org>
2011-09-09 16:56:56 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunkyTarro::Application.config.secret_token = '1f6d5c0ab62f1ca1e097f1603a6664929d90bcb1ec5e9630927da98cee336d9643027a561495b847a0269680b621e81f6bca2a44ffd5f2e2fb3aee5e33e994fd'
